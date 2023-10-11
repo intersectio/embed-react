9e7031d8ff9f086ef9e9687f5357625635405c52
dpayLuna <athenax0708@gmail.com>
2023-10-12 03:14:34 +0800

    upload from local

---
+++ src/FullPageChat.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from 'react'
+import type { BotProps } from 'cubieai-embed'
+
+type Props = BotProps & {
+  style?: React.CSSProperties
+  className?: string
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'cubieai-fullchatbot': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & { class?: string }
+    }
+  }
+}
+
+type FullPageChatElement = HTMLElement & Props
+
+export const FullPageChat = ({ style, className, ...assignableProps }: Props) => {
+  const ref = useRef<FullPageChatElement | null>(null)
+
+  useEffect(() => {
+    ;(async () => {
+      await import('cubieai-embed/dist/web')
+    })()
+  }, [])
+
+  useEffect(() => {
+    if (!ref.current) return
+    Object.assign(ref.current, assignableProps)
+  }, [assignableProps])
+
+  return <cubieai-fullchatbot ref={ref} style={style} class={className} />
+}
